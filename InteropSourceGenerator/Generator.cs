24b6d27cfe96fad91d9dfd5579c263daaf56b3de
neugolok@outlook.com <neugolok@outlook.com>
2024-08-27 10:27:37 +0400

    Add project files.

---
+++ InteropSourceGenerator/Generator.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace InteropSourceGenerator
+{
+    [Generator]
+    public class Generator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var code = @"
+            namespace Test
+            {
+                public static class Welcome 
+                {
+                    public static void Print() => Console.WriteLine($""Hello World"");
+                }
+            }";
+            context.AddSource("Test.welcome.generated.cs", code);
+        }
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            
+        }
+    }
+}
